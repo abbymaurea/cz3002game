f1a7140dae20717781f29ae12008efe53074c554
Abby Maurea <abbymaurea@gmail.com>
2020-10-09 12:03:28 +0800

    Updates

---
+++ precache-manifest.6ca42dc2de5a2d7bee109bf4570a27f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdcc2c86f9d1491cac4e03b2f64ff3e",
+    "url": "/cz3002game/index.html"
+  },
+  {
+    "revision": "85d626868edeca815381",
+    "url": "/cz3002game/static/css/main.9afecbd1.chunk.css"
+  },
+  {
+    "revision": "44ae43b3bd1a95e84061",
+    "url": "/cz3002game/static/js/2.462e066a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cz3002game/static/js/2.462e066a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d626868edeca815381",
+    "url": "/cz3002game/static/js/main.bc5393da.chunk.js"
+  },
+  {
+    "revision": "49130122ac30d335171f",
+    "url": "/cz3002game/static/js/runtime-main.7047dde6.js"
+  },
+  {
+    "revision": "e3789c2b8685b3be93d34b4869b8196d",
+    "url": "/cz3002game/static/media/button_press.e3789c2b.mp3"
+  }
+]);
